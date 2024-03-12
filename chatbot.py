d6d7439db6eab2b99b4da6d632aad911918dc1d4
muzexun-T <2579827479@qq.com>
2024-03-12 14:37:19 +0800

    fix chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,12 +1,15 @@
-from telegram import Update
-from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, CallbackContext)
-#import configparser
 import os
+import telegram
+from telegram.ext import Updater, MessageHandler, Filters
+#import configparser
 import logging
-import redis
-from CHATGPT_HKBU import HKBU_ChatGPT
+# import redis
+# from CHATGPT_HKBU import HKBU_ChatGPT
 
-global redis1
+
+import requests
+
+
 def main():
     # Load your token and create an Updater for your Bot
     # config = configparser.ConfigParser()
@@ -14,7 +17,7 @@ def main():
     # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     updater=Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
-    global redis1
+    # global redis1
     # redis1 = redis.Redis(host=(config['REDIS']['HOST']),
     #             password=(config['REDIS']['PASSWORD']),
     #             port=(config['REDIS']['REDISPORT']))
@@ -29,37 +32,37 @@ def main():
     # dispatcher.add_handler(CommandHandler("help", help_command))
     global chatgpt
     #chatgpt = HKBU_ChatGPT(config)
-    chatgpt = HKBU_ChatGPT()
+    chatgpt = HKBU_GPT()
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
     # To start the bot:
     updater.start_polling()
     updater.idle()
 
-def echo(update, context):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
+# def echo(update, context):
+#     reply_message = update.message.text.upper()
+#     logging.info("Update: " + str(update))
+#     logging.info("context: " + str(context))
+#     context.bot.send_message(chat_id=update.effective_chat.id, text= reply_message)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-def help_command(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /help is issued."""
-    update.message.reply_text('Helping you helping you.')
+# # Define a few command handlers. These usually take the two arguments update and
+# # context. Error handlers also receive the raised TelegramError object in error.
+# def help_command(update: Update, context: CallbackContext) -> None:
+#     """Send a message when the command /help is issued."""
+#     update.message.reply_text('Helping you helping you.')
 
 
-def add(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /add is issued."""
-    try:
-        global redis1
-        logging.info(context.args[0])
-        msg = context.args[0]   # /add keyword <-- this should store the keyword
-        redis1.incr(msg)
-        update.message.reply_text('You have said ' + msg +  ' for ' +
-                        redis1.get(msg).decode('UTF-8') + ' times.')
-    except (IndexError, ValueError):
-        update.message.reply_text('Usage: /add <keyword>')
+# def add(update: Update, context: CallbackContext) -> None:
+#     """Send a message when the command /add is issued."""
+#     try:
+#         global redis1
+#         logging.info(context.args[0])
+#         msg = context.args[0]   # /add keyword <-- this should store the keyword
+#         redis1.incr(msg)
+#         update.message.reply_text('You have said ' + msg +  ' for ' +
+#                         redis1.get(msg).decode('UTF-8') + ' times.')
+#     except (IndexError, ValueError):
+#         update.message.reply_text('Usage: /add <keyword>')
 
 def equiped_chatgpt(update, context):
     global chatgpt
